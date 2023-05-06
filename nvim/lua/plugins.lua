c7a5dafe8ac1dd2c8bcd78084de2e015fb3bd0b7
Daniel Swensson <daniel@danielswensson.se>
2023-05-06 21:39:09 +0200

    add treesitter

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,21 +1,27 @@
 return require('packer').startup(function(use)
-	-- Packer can manage itself
-	use 'wbthomason/packer.nvim'
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
 
-	use 'tpope/vim-fugitive'
-	use 'jremmen/vim-ripgrep'
-	use 'vim-autoformat/vim-autoformat'
-	use 'nvim-lua/plenary.nvim'
-	use 'github/copilot.vim'
-	use 'christoomey/vim-tmux-navigator'
-	use 'nvim-lualine/lualine.nvim'
-	use 'nvim-tree/nvim-web-devicons'
-	use 'neovim/nvim-lspconfig'
-	use {
-		'nvim-telescope/telescope.nvim', tag = '0.1.1',
-		requires = { {'nvim-lua/plenary.nvim'} }
-	}
-	use 'ellisonleao/gruvbox.nvim'
-	use 'sbdchd/neoformat'
+  use 'tpope/vim-fugitive'
+  use 'jremmen/vim-ripgrep'
+  use 'github/copilot.vim'
+  use 'christoomey/vim-tmux-navigator'
+  use 'nvim-lualine/lualine.nvim'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'neovim/nvim-lspconfig'
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    requires = { {'nvim-lua/plenary.nvim'} }
+  }
+  use 'ellisonleao/gruvbox.nvim'
+  use 'sbdchd/neoformat'
+  use {
+    'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  }
+  use 'nvim-treesitter/nvim-treesitter-context'
 end)
 
